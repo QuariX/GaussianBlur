8610ebe340dc494e51e9b76188a2364d07400513
QuariX <antonim.n1.a@gmail.com>
2021-11-25 17:36:39 +0200

    Revert "add program running time"
    This reverts commit c26d7dd521744b24f5e0e23f4e5cdd24383527fd.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,7 +4,6 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.time.LocalTime;
 
 /**
  * @author QuariX
@@ -16,46 +15,24 @@ import java.time.LocalTime;
 public class Main {
     public static void main(String[] args) {
 
-        LocalTime start = LocalTime.now();
         int rad = 25;
         double[][] weights = GaussianBlur.getInstance().generateWeightMatrix(rad, Math.sqrt(rad));
         GaussianBlur.getInstance().printWeightedMatrixToFile(weights);
 
         BufferedImage answer = null;
         try {
-            BufferedImage source_img = ImageIO.read(new File("src/ImgInput/21.png"));
+            BufferedImage source_img = ImageIO.read(new File("src/ImgInput/11.jpg"));
             answer = GaussianBlur.getInstance().createGaussianImage(source_img, weights, rad);
         } catch (IOException e) {
             e.printStackTrace();
         }
         try {
             assert answer != null;
-            ImageIO.write(answer, "PNG", new File("src/ImgOutput/22.png"));
+            ImageIO.write(answer, "PNG", new File("src/ImgOutput/12.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        LocalTime end = LocalTime.now();
-        int startHour = start.getHour();
-        int startMinute = start.getMinute();
-        int startSecond = start.getSecond();
-        int endHour = end.getHour();
-        int endMinute = end.getMinute();
-        int endSecond = end.getSecond();
-        if (endSecond < startSecond) {
-            endMinute = endMinute - 1;
-            endSecond = endSecond + 60;
-        }
-        int h = endHour - startHour;
-        int m = endMinute - startMinute;
-        int s = endSecond - startSecond;
-        if (s == 60) {
-            s = 0;
-            m = m + 1;
-        }
-        System.out.println("Start time: " + start);
-        System.out.println("End time: " + end);
-        System.out.println("Program running time: " + h +":"+ m +":" + s);
         System.out.println("DONE!");
     }
 }
