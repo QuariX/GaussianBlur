c21eba4b1e08ec2efbc1b09d36164e1ce6018750
QuariX <antonim.n1.a@gmail.com>
2021-11-25 18:12:45 +0200

    Add program running time

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,6 +4,7 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalTime;
 
 /**
  * @author QuariX
@@ -15,24 +16,56 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
 
+        LocalTime start = LocalTime.now();
         int rad = 25;
         double[][] weights = GaussianBlur.getInstance().generateWeightMatrix(rad, Math.sqrt(rad));
         GaussianBlur.getInstance().printWeightedMatrixToFile(weights);
 
         BufferedImage answer = null;
         try {
-            BufferedImage source_img = ImageIO.read(new File("src/ImgInput/11.jpg"));
+            BufferedImage source_img = ImageIO.read(new File("src/ImgInput/21.png"));
             answer = GaussianBlur.getInstance().createGaussianImage(source_img, weights, rad);
         } catch (IOException e) {
             e.printStackTrace();
         }
         try {
             assert answer != null;
-            ImageIO.write(answer, "PNG", new File("src/ImgOutput/12.png"));
+            ImageIO.write(answer, "PNG", new File("src/ImgOutput/22.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        LocalTime finish = LocalTime.now();
+        int startHour = start.getHour();
+        int startMinute = start.getMinute();
+        int startSecond = start.getSecond();
+        int finishHour = finish.getHour();
+        int finishMinute = finish.getMinute();
+        int finishSecond = finish.getSecond();
+        if (finishSecond < startSecond) {
+            --finishMinute;
+            finishSecond += 60;
+        }
+        if (finishMinute < startMinute) {
+            --finishHour;
+            finishMinute += 60;
+        }
+
+        int totalHour = finishHour - startHour;
+        int totalMinute = finishMinute - startMinute;
+        int totalSecond = finishSecond - startSecond;
+        if (totalSecond == 60) {
+            totalSecond = 0;
+            ++totalMinute;
+        }
+        if (totalMinute == 60) {
+            totalMinute = 0;
+            ++totalHour;
+        }
+
+        System.out.println("Start time: " + start);
+        System.out.println("End time: " + finish);
+        System.out.println("Program running time: " + totalHour + ":" + totalMinute + ":" + totalSecond);
         System.out.println("DONE!");
     }
 }
